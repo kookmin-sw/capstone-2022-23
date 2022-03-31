e10fd46d487adf060f846f4be5775b70a4969b6e
sooyeon-kr <sooyeon7027@naver.com>
2022-03-31 14:19:44 +0900

    Chore: Set up the development environment for frontend and backend projects

--- code/backend/urls.py
+++ code/backend/urls.py
@@ -1,21 +1,18 @@
-"""backend URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
+
+# MEDIA_URL로 시작되는 요청이 오면 MEDIA_ROOT에서 파일을 찾아 serving 한다. static()리스트 반환
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    urlpatterns += [
+        path(r"^__debug__/", include(debug_toolbar.urls)),
+    ]

36b3921e33bad0ddf1409f6be23b9d77ef366a55
sooyeon-kr <sooyeon7027@naver.com>
2022-05-10 14:36:22 +0900

    Chore:rearrange the web directory structure

--- web/src/web/settings/common.py
+++ web/src/web/settings/common.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path, os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # env = os.environ.Env(DEBUG=(bool),  False)
 # reading .env file
 # os.environ.Env.read_env(env_file=os.path.join(BASE_DIR.parent, ".envs/.django"))
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "celery",
+    "apps.accounts",
 ]
 
 MIDDLEWARE = [
@@ -59,7 +60,7 @@ ROOT_URLCONF = "web.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [

29af24454ee8a4a2ac6393c2c7e5e5955abfadbd
sooyeon-kr <sooyeon7027@naver.com>
2022-05-11 21:35:08 +0900

    Fix url path

--- web/src/apps/decider/views.py
+++ web/src/apps/decider/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required
+def mood_search(request):
+    return render(
+        request,
+        "search.html",
+    )
+
+
+@login_required
+def mood_result(request):
+    return render(
+        request,
+        "result.html",
+    )

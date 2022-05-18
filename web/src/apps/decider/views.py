531349bf13ddfb6ff98d91247ef4504cfd0e1da8
sooyeon-kr <sooyeon7027@naver.com>
2022-05-19 02:16:16 +0900

    Refactor:crawling func

--- web/src/apps/decider/views.py
+++ web/src/apps/decider/views.py
@@ -1,19 +1,34 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from . import tasks
-
+from .models import Site, Result
+from django.conf import settings
 
 @login_required
 def mood_search(request):
-    bar = tasks.crawl()
-    taewon = tasks.get_tokenized()
-    context = {"foo": bar, "taewon": taewon}
-    return render(request, "search.html", context)
+    # bar = tasks.crawl()
+    # taewon = tasks.get_crawling_text_return_mood()
+    # context = {"taewon": taewon}
+    # return render(request, "search.html", context)
+    return render(request, "search.html")
 
 
 @login_required
-def mood_result(request):
+def mood_result(request, pk):
+    if request.method == 'POST':
+        url = request.POST['input_url']
+        user = request.user
+        site = Site(url=url)
+        site.save()
+        Result.objects.create(url=site, user=user)
+        result = Result.objects.all()
+        only_text = tasks.get_crawling_text_return_mood(url)
+        # context = {"target": tast_result}
+        
+        
+    
+    user_result_list = Result.objects.filter(user=request.user)
     return render(
         request,
-        "result.html",
+        "result.html", {'result_list':user_result_list, "only_text":only_text}
     )

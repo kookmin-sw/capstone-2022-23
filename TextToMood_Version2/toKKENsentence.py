fb1f07d4007c812e33a3d57115eab9eaa66abe6b
wellbeing98 <54866346+wellbeing98@users.noreply.github.com>
2022-05-23 18:50:18 +0900

    update

--- TextToMood_Version2/toKKENsentence.py
+++ TextToMood_Version2/toKKENsentence.py
@@ -1,4 +1,5 @@
 
+
 #2클롤링한 데이터를 문장 단위로 분리합니다. 
 import copy
 from konlpy.tag import Kkma
@@ -7,151 +8,23 @@ import pickle
 import os
 i=0
 sentence =[]
-for filename in os.listdir('/home/wjdxodnjs/MusicDecider/MusicdeciderEmotions/관능'):
-   with open(os.path.join("/home/wjdxodnjs/MusicDecider/MusicdeciderEmotions/관능", filename), 'r') as f:
-       text = f.read()
-    #    print(text)
-       print('\n ' + str(i))
-       i = i+1
-       token = kkma.sentences(text)
-       print(token)
-        
-       for index in range(len(token)):
-           sentence.append(token[index])
-for filename in os.listdir('/home/wjdxodnjs/MusicDecider/MusicdeciderEmotions/기쁨'):
-   with open(os.path.join("/home/wjdxodnjs/MusicDecider/MusicdeciderEmotions/기쁨", filename), 'r') as f:
-       text = f.read()
-    #    print(text)
-       print('\n ' + str(i))
-       i = i+1
-       token = kkma.sentences(text)
-       print(token)
-        
-       for index in range(len(token)):
-           sentence.append(token[index])
-for filename in os.listdir('/home/wjdxodnjs/MusicDecider/MusicdeciderEmotions/두려움'):
-   with open(os.path.join("/home/wjdxodnjs/MusicDecider/MusicdeciderEmotions/두려움", filename), 'r') as f:
-       text = f.read()
-    #    print(text)
-       print('\n ' + str(i))
-       i = i+1
-       token = kkma.sentences(text)
-       print(token)
-        
-       for index in range(len(token)):
-           sentence.append(token[index])
-for filename in os.listdir('/home/wjdxodnjs/MusicDecider/MusicdeciderEmotions/몽환'):
-   with open(os.path.join("/home/wjdxodnjs/MusicDecider/MusicdeciderEmotions/몽환", filename), 'r') as f:
-       text = f.read()
-    #    print(text)
-       print('\n ' + str(i))
-       i = i+1
-       token = kkma.sentences(text)
-       print(token)
-        
-       for index in range(len(token)):
-           sentence.append(token[index])
-for filename in os.listdir('/home/wjdxodnjs/MusicDecider/MusicdeciderEmotions/반항'):
-   with open(os.path.join("/home/wjdxodnjs/MusicDecider/MusicdeciderEmotions/반항", filename), 'r') as f:
-       text = f.read()
-    #    print(text)
-       print('\n ' + str(i))
-       i = i+1
-       token = kkma.sentences(text)
-       print(token)
-        
-       for index in range(len(token)):
-           sentence.append(token[index])
-for filename in os.listdir('/home/wjdxodnjs/MusicDecider/MusicdeciderEmotions/불안'):
-   with open(os.path.join("/home/wjdxodnjs/MusicDecider/MusicdeciderEmotions/불안", filename), 'r') as f:
-       text = f.read()
-    #    print(text)
-       print('\n ' + str(i))
-       i = i+1
-       token = kkma.sentences(text)
-       print(token)
-        
-       for index in range(len(token)):
-           sentence.append(token[index])
-for filename in os.listdir('/home/wjdxodnjs/MusicDecider/MusicdeciderEmotions/승리'):
-   with open(os.path.join("/home/wjdxodnjs/MusicDecider/MusicdeciderEmotions/승리", filename), 'r') as f:
-       text = f.read()
-    #    print(text)
-       print('\n ' + str(i))
-       i = i+1
-       token = kkma.sentences(text)
-       print(token)
-        
-       for index in range(len(token)):
-           sentence.append(token[index])
-for filename in os.listdir('/home/wjdxodnjs/MusicDecider/MusicdeciderEmotions/신남'):
-   with open(os.path.join("/home/wjdxodnjs/MusicDecider/MusicdeciderEmotions/신남", filename), 'r') as f:
-       text = f.read()
-    #    print(text)
-       print('\n ' + str(i))
-       i = i+1
-       token = kkma.sentences(text)
-       print(token)
-        
-       for index in range(len(token)):
-           sentence.append(token[index])
-for filename in os.listdir('/home/wjdxodnjs/MusicDecider/MusicdeciderEmotions/아름다움'):
-   with open(os.path.join("/home/wjdxodnjs/MusicDecider/MusicdeciderEmotions/아름다움", filename), 'r') as f:
-       text = f.read()
-    #    print(text)
-       print('\n ' + str(i))
-       i = i+1
-       token = kkma.sentences(text)
-       print(token)
-        
-       for index in range(len(token)):
-           sentence.append(token[index])
-for filename in os.listdir('/home/wjdxodnjs/MusicDecider/MusicdeciderEmotions/이별'):
-   with open(os.path.join("/home/wjdxodnjs/MusicDecider/MusicdeciderEmotions/이별", filename), 'r') as f:
-       text = f.read()
-    #    print(text)
-       print('\n ' + str(i))
-       i = i+1
-       token = kkma.sentences(text)
-       print(token)
-        
-       for index in range(len(token)):
-           sentence.append(token[index])
-for filename in os.listdir('/home/wjdxodnjs/MusicDecider/MusicdeciderEmotions/짜증'):
-   with open(os.path.join("/home/wjdxodnjs/MusicDecider/MusicdeciderEmotions/짜증", filename), 'r') as f:
-       text = f.read()
-    #    print(text)
-       print('\n ' + str(i))
-       i = i+1
-       token = kkma.sentences(text)
-       print(token)
-        
-       for index in range(len(token)):
-           sentence.append(token[index])
-for filename in os.listdir('/home/wjdxodnjs/MusicDecider/MusicdeciderEmotions/평안'):
-   with open(os.path.join("/home/wjdxodnjs/MusicDecider/MusicdeciderEmotions/평안", filename), 'r') as f:
-       text = f.read()
-    #    print(text)
-       print('\n ' + str(i))
-       i = i+1
-       token = kkma.sentences(text)
-       print(token)
-        
-       for index in range(len(token)):
-           sentence.append(token[index])
-for filename in os.listdir('/home/wjdxodnjs/MusicDecider/MusicdeciderEmotions/활력'):
-   with open(os.path.join("/home/wjdxodnjs/MusicDecider/MusicdeciderEmotions/활력", filename), 'r') as f:
-       text = f.read()
-    #    print(text)
-       print('\n ' + str(i))
-       i = i+1
-       token = kkma.sentences(text)
-       print(token)
-        
-       for index in range(len(token)):
-           sentence.append(token[index])
+lyric_emotion = ['관능', '기쁨', '두려움', '몽환','반항','불안','승리','신남','아름다움','이별','짜증','편안','활력']
+for i in lyric_emotion:
+   for filename in os.listdir('/home/wjdxodnjs/MusicDecider/MusicdeciderEmotions/'+i):
+      with open(os.path.join("/home/wjdxodnjs/MusicDecider/MusicdeciderEmotions/"+i, filename), 'r') as f:
+         text = f.read()
+      #    print(text)
+         print('\n ' + str(i))
+         i = i+1
+         token = kkma.sentences(text)
+         print(token)
+         
+         for index in range(len(token)):
+            sentence.append(token[index])
+
 with open('list3.pickle', 'wb') as f:
     pickle.dump(sentence, f, pickle.HIGHEST_PROTOCOL)
 
 f.close()
+
 
